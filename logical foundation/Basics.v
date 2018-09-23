51826512395b3e2bd534c755bafd42dc3cf9bcc3
fsw <shiwei_feng@icloud.com>
2018-09-23 16:05:24 +0800

    change

--- logical foundation/Basics.v
+++ logical foundation/Basics.v
@@ -725,7 +725,7 @@ Proof. simpl. reflexivity.  Qed.
 Example test_blt_nat2:             (blt_nat 2 4) = true.
 Proof. simpl. reflexivity.  Qed.
 Example test_blt_nat3:             (blt_nat 4 2) = false.
-
+Proof. simpl. reflexivity.  Qed.
 (** [] *)
 
 (* ################################################################# *)
